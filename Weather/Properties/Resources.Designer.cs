f6d5672f45a8b9c0a4d6f611c7b9040a985a8e33
YJK <yjk@yjk.name>
2020-12-18 11:14:59 +0800

    整理代码

--- Weather/Properties/Resources.Designer.cs
+++ Weather/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Weather.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -47,8 +47,8 @@ namespace Weather.Properties {
         }
         
         /// <summary>
-        ///   使用此强类型资源类，为所有资源查找
-        ///   重写当前线程的 CurrentUICulture 属性。
+        ///   重写当前线程的 CurrentUICulture 属性，对
+        ///   使用此强类型资源类的所有资源查找执行重写。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Globalization.CultureInfo Culture {
@@ -80,7 +80,7 @@ namespace Weather.Properties {
         ///101020200,闵行,上海,上海
         ///101020300,宝山,上海,上海
         ///101020500,嘉定,上海,上海
-        ///101020600,南汇,上海,上海
+        ///101020600,浦东,上海,上海
         ///101020700,金山,上海,上海
         ///101020800,青浦,上海,上海
         ///101020900,松江,上海,上海
